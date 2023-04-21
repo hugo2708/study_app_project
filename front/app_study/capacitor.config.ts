bdcdf3908bb9978f44b0eafa25fc5e83ce5d44e8
Jose Aguilera <joseaguilera1709@gmail.com>
2023-04-21 15:57:59 -0400

    Primera version front

--- front/app_study/capacitor.config.ts
+++ front/app_study/capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'app_study',
+  appName: 'blanck',
   webDir: 'www',
   bundledWebRuntime: false
 };
--- front/app_study/src/app/page/profile/profile.component.ts
+++ front/app_study/src/app/page/profile/profile.component.ts
@@ -1,14 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss'],
 })
-export class ProfileComponent  implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {}
-
+export class ProfileComponent {
+  loggedInUser = "Juan Perez";
+  loggedInUserEmail = "juan@example.com";
+  loggedInUserPhone = "555-1234";
+  loggedInUserAddress = "Calle 123, Ciudad";
 }
+
+
