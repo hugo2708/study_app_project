fc0dbf9fab0ff7ffecc4ede37510c0320cb1ca17
Jose Aguilera <joseaguilera1709@gmail.com>
2023-05-16 21:44:45 -0400

    Ajustes del back

--- backend/src/controller/users.controller.js
+++ backend/src/controller/users.controller.js
@@ -1,5 +1,5 @@
 const UserService = require("../service/users.service");
-
+const jwt = require ('jsonwebtoken');
 const listar = async function (req, res) {
     console.log("listar usuarios");
 
@@ -91,7 +91,66 @@ const eliminar = async function (req, res) {
     }
 };
 
+const login = async function (req, res){
+    console.log("login usuario");
+    try {
+        const userDB = await sequelize.query 
+        ("SELECT * FROM users WHERE email = '" + req.body.email + "' AND password =  '" + req.body.password + "'");
+    console.log("users", userDB);
+    let user = null;
+    if(userDB.length > 0 && userDB[0].length > 0){
+        user = userDB[0][0];
+        if(user.token){
+            res.json({
+                success: false,
+                error: "usuario ya esta autenticado"
+            });
+            return;
+        }
+        let token = jwt.sign({
+            codigo: user.codigo,
+            name: user.name,
+            last_name: user.list_name,
+            avatar: user.avatar,
+            email: user.email
+        }, 'passwd');
+        const userDBupdate = await sequelize.query("UPDATE users SET token = '" + token + "' WHERE id =  " + user.id );
+        res.json({
+            success: true,
+            token
+        })
+    }else{
+        res.json({
+            success: false,
+            error: "usuario no encontrado"
+        });
+    }
+    } catch (error) {
+        console.log(error);
+        res.json({
+            success: false,
+            error: error.message
+        });
+    }
+}
+
+const logout = async function (res, res){
+    try {
+        const userDB = await sequelize.query("UPDATE users SET token = null WHERE id =  " + res.locals.userId + " " );
+
+        res.json({
+            success: true
+        })
+    } catch (error) {
+        console.log(error);
+        res.json({
+            success: false,
+            error: error.message
+        })
+    }
+}
+
 
 module.exports = {
-    listar, consultarPorCodigo, actualizar, eliminar
+    listar, consultarPorCodigo, actualizar, eliminar, login, logout
 };
--- backend/src/route/users.route.js
+++ backend/src/route/users.route.js
@@ -5,4 +5,6 @@ module.exports = function (app) {
     app.get("/user/:id", userController.consultarPorCodigo);
     app.post("/users/update", userController.actualizar);
     app.delete("/users/delete/:id", userController.eliminar);
+    app.post("/user/login", userController.login);
+    app.post("/user/logout", userController.logout);
 }
